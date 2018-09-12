23c77829c39e21b4b7eccadb9d27f50e5abfe5c6
oknowa2535 <raulsfreitas1997@hotmail.com>
2018-09-11 21:00:02 -0300

    Adicionando arquivos MCU
    Signed-off-by: oknowa2535 <raulsfreitas1997@hotmail.com>

---
+++ MCU/Comedouro/Debug/driverlib/MSP430F5xx_6xx/ucs.d
@@ -0,0 +1,43 @@
+# FIXED
+
+driverlib/MSP430F5xx_6xx/ucs.obj: ../driverlib/MSP430F5xx_6xx/ucs.c
+driverlib/MSP430F5xx_6xx/ucs.obj: ../driverlib/MSP430F5xx_6xx/inc/hw_memmap.h
+driverlib/MSP430F5xx_6xx/ucs.obj: /home/raul/ti/ccsv8/ccs_base/msp430/include/msp430.h
+driverlib/MSP430F5xx_6xx/ucs.obj: /home/raul/ti/ccsv8/ccs_base/msp430/include/msp430f5529.h
+driverlib/MSP430F5xx_6xx/ucs.obj: /home/raul/ti/ccsv8/ccs_base/msp430/include/in430.h
+driverlib/MSP430F5xx_6xx/ucs.obj: /home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/intrinsics.h
+driverlib/MSP430F5xx_6xx/ucs.obj: /home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/intrinsics_legacy_undefs.h
+driverlib/MSP430F5xx_6xx/ucs.obj: /home/raul/ti/ccsv8/ccs_base/msp430/include/msp430f5xx_6xxgeneric.h
+driverlib/MSP430F5xx_6xx/ucs.obj: /home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/stdint.h
+driverlib/MSP430F5xx_6xx/ucs.obj: /home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/sys/stdint.h
+driverlib/MSP430F5xx_6xx/ucs.obj: /home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/sys/cdefs.h
+driverlib/MSP430F5xx_6xx/ucs.obj: /home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/sys/_types.h
+driverlib/MSP430F5xx_6xx/ucs.obj: /home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/machine/_types.h
+driverlib/MSP430F5xx_6xx/ucs.obj: /home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/machine/_stdint.h
+driverlib/MSP430F5xx_6xx/ucs.obj: /home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/sys/_stdint.h
+driverlib/MSP430F5xx_6xx/ucs.obj: /home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/stdbool.h
+driverlib/MSP430F5xx_6xx/ucs.obj: ../driverlib/MSP430F5xx_6xx/ucs.h
+driverlib/MSP430F5xx_6xx/ucs.obj: /home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/assert.h
+driverlib/MSP430F5xx_6xx/ucs.obj: /home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/_ti_config.h
+driverlib/MSP430F5xx_6xx/ucs.obj: /home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/linkage.h
+
+../driverlib/MSP430F5xx_6xx/ucs.c: 
+../driverlib/MSP430F5xx_6xx/inc/hw_memmap.h: 
+/home/raul/ti/ccsv8/ccs_base/msp430/include/msp430.h: 
+/home/raul/ti/ccsv8/ccs_base/msp430/include/msp430f5529.h: 
+/home/raul/ti/ccsv8/ccs_base/msp430/include/in430.h: 
+/home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/intrinsics.h: 
+/home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/intrinsics_legacy_undefs.h: 
+/home/raul/ti/ccsv8/ccs_base/msp430/include/msp430f5xx_6xxgeneric.h: 
+/home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/stdint.h: 
+/home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/sys/stdint.h: 
+/home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/sys/cdefs.h: 
+/home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/sys/_types.h: 
+/home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/machine/_types.h: 
+/home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/machine/_stdint.h: 
+/home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/sys/_stdint.h: 
+/home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/stdbool.h: 
+../driverlib/MSP430F5xx_6xx/ucs.h: 
+/home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/assert.h: 
+/home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/_ti_config.h: 
+/home/raul/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.2.LTS/include/linkage.h: 
