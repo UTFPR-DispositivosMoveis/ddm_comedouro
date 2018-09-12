da6e816d0d287755a22562eb1fc286f920e50b2c
jeffersonwgoes <jeffersonwgoes@gmail.com>
2018-09-12 19:59:46 -0300

    ta ai

--- Middle/server.c
+++ Middle/server.c
@@ -10,12 +10,6 @@
 #define MAX_MSG 1024
 #define PORTA 12345
 
-/*
- Servidor recebe mensagem do cliente, imprime na tela e envia uma resposta.
- Se o cliente desconectar, processo do servidor continua ativo esperando
- por novas conexoes.
- */
-
 int main(void){
 	//variaveis
 	int socket_desc , conexao , c;
@@ -27,12 +21,6 @@ int main(void){
 	// destinado a armazenar o IP e porta do cliente
 	char *client_ip;
 	int client_port;
-
-	/*********************************************************/
-        //Passo 1: Criar o socket servidor na porta passada por parametro
-	// AF_INET = ARPA INTERNET PROTOCOLS
-	// SOCK_STREAM = TCP
-	// 0 = protocolo IP
 
 	socket_desc = socket(AF_INET , SOCK_STREAM , 0);
 	if (socket_desc == -1) {
@@ -49,7 +37,7 @@ int main(void){
 	if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0){
 		puts("Erro ao fazer bind!!! \n");
 	}
-	puts("Bind efetuado com sucesso!!! \n");
+
 
         // Passo 2: aguarda por conexões de clientes
         // O segundo parametro indica quantos conexoes podem aguardar no buffer
@@ -57,7 +45,6 @@ int main(void){
 	/*********************************************************/
 
 	//Aceitando e tratando conexoes
-	puts("Aguardando por conexoes...");
 	c = sizeof(struct sockaddr_in);
 	// Fica esperando por conexoes
 	while( (conexao = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c)) ){;
@@ -70,7 +57,6 @@ int main(void){
 		// pegando IP e porta do cliente
 		client_ip = inet_ntoa(client.sin_addr);
 		client_port = ntohs(client.sin_port);
-		printf("Cliente conectou: %s : [ %d ]\n",client_ip, client_port);
 
 		// lendo dados enviados pelo cliente
 		if((tamanho = read(conexao,resposta, MAX_MSG)) < 0){
@@ -80,7 +66,7 @@ int main(void){
 
 		// Coloca terminador de string
 		resposta[tamanho] = '\0';
-		printf("O cliente falou: %s \n", resposta);
+		printf("%s[%d]: %s \n", client_ip, client_port, resposta);
 
 		// Enviando resposta para o cliente
 		write(conexao , resposta , strlen(resposta));
@@ -90,9 +76,4 @@ int main(void){
 		close(conexao);
 	}//fim do while
 
-	/*********************************************************/
-
-
-	printf("Servidor finalizado...\n");
-	return 0;
 }
