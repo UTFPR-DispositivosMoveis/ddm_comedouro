b3877058567b1e28202f343b9359158535544288
raulscr <raulfreitas@alunos.utfpr.edu.br>
2018-10-06 22:08:43 -0300

    limpando as imundice do código do motor (2) --Fazer o parser genérico em C --Terminar de organizar o IO

--- MCU/Comedouro/IO/main.c
+++ MCU/Comedouro/IO/main.c
@@ -18,7 +18,6 @@ Timer_A_outputPWMParam param = {0}; //Motor
 Timer_A_outputPWMParam param2 = {0}; //Buzzer
 Timer_A_initContinuousModeParam paramUP = {0}; //TimerA0 p/ ultrassonico
 
-uint32_t ACLK, MCLK, SMCLK;
 int cont =0, FlagMedir=0;
 uint16_t upCounter = 0;
 uint16_t distancia = 0;
@@ -27,70 +26,10 @@ int NivelRacao = 0; //Altura � de 60cm (1-> 4a15cm, 2->15a30, 3->30a45, 4-> 45
 int dis;
 int echo_pulse_duration;
 
-void getClockFreq(){
-    ACLK = UCS_getACLK();
-    SMCLK = UCS_getSMCLK();
-    MCLK = UCS_getMCLK();
-}
-
-void Girar_Motor(){
-
-    Timer_A_stop(TIMER_A1_BASE);
-
-    if (DUTYv==0)
-        DUTYv = 26; //4% duty
-
-    else if (DUTYv==26) //4% duty
-        DUTYv = 52; //8% duty
-
-    else if (DUTYv==52)
-        DUTYv = 26;
-
-    param.dutyCycle = DUTYv;
-    Timer_A_outputPWM(TIMER_A1_BASE, &param);
-}
-
-void set_BuzzerOn(){
-    Timer_A_stop(TIMER_A2_BASE);
-    TIMER_PERIOD_BUZZER = 32768 / FREQ_SOM;
-    param2.timerPeriod = TIMER_PERIOD_BUZZER;
-    param2.dutyCycle = param2.timerPeriod / 2;
-    Timer_A_outputPWM(TIMER_A2_BASE, &param2);
-}
-
-void set_BuzzerOff(){
-    Timer_A_stop(TIMER_A2_BASE);
-}
-
-int get_Distance(){
-    /*
-     * Gera o pulso de 10 uS
-     */
-    GPIO_setOutputHighOnPin (GPIO_PORT_P4, GPIO_PIN3);
-    __delay_cycles (264);
-    GPIO_setOutputLowOnPin (GPIO_PORT_P4, GPIO_PIN3);
-
-    while ((P1IN & BIT2)==0);   // Wait for start of echo pulse
-
-    echo_pulse_duration = Timer_A_getCounterValue (TIMER_A0_BASE);
-
-    while ((P1IN & BIT2)>0);    // Wait for end of echo pulse
-
-    if(Timer_A_getCounterValue (TIMER_A0_BASE) > echo_pulse_duration)
-        echo_pulse_duration = Timer_A_getCounterValue (TIMER_A0_BASE) - echo_pulse_duration;
-    else
-        echo_pulse_duration = 65535 - echo_pulse_duration + Timer_A_getCounterValue (TIMER_A0_BASE);
-
-    //retorna distancia em mm
-    return 0.017 * echo_pulse_duration;
-
-}
-
 
 void main (void){
    WDT_A_hold(WDT_A_BASE);
     Config_BCS(24000);
-    getClockFreq ();
     Config_TimerA ();
     Config_RTC ();
     while(true){
